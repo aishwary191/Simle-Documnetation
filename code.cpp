a3e3085adb850d9015124f5e662273846ac8e579
aishwary191 <51899967+aishwary191@users.noreply.github.com>
2019-06-24 01:23:31 +0530

    div implemented

--- code.cpp
+++ code.cpp
@@ -1,6 +1,10 @@
+int implementdiv(int a,int b){
+  return a/b;
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
 cout<<"hello world"<<endl;
+int res=implementdiv(8,4);
+  cout<<res<<endl;
 
 }
